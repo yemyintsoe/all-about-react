2ec477ce2741fbec1c945de5224db3f8bc163fd0
Ye Myint Soe <52601159+yemyintsoe@users.noreply.github.com>
2023-01-28 13:11:00 +0630

    Update 03.useState with array.js

--- hooks/03.useState with array.js
+++ hooks/03.useState with array.js
@@ -1,1 +1,53 @@
-// kasdhifskdfh skdfhf
+import React, { useState } from "react"
+
+const UseStateWithArr = () => {
+  // useState with array
+  let [items, setItems] = useState([
+    {id: 1, name: 'apple'},
+    {id: 2, name: 'orange'},
+    {id: 3, name: 'banana'},
+    {id: 4, name: 'pineapple'},
+  ])
+
+  // create
+  const nameInput = React.createRef('')
+  const addItem = (e) => {
+    e.preventDefault()
+    const id = Math.floor(Math.random() * 100000) + 1
+    const name = nameInput.current.value
+    if(name !== '') {
+      // this is the main reason of useState with Array
+      setItems([...items, {id, name}])
+      nameInput.current.value = ''
+    }else {
+      alert('the name field is required')
+    }
+  }
+
+  // delete
+  const deleteItem = (id) => {
+    setItems(items = items.filter((item) => {
+      return item.id !== id
+    }))
+  }
+
+  return (
+    <div>
+        <ul>
+          {
+              items.map(item => (
+                <li key={item.id}>
+                  <b>Item:</b> {item.id} - {item.name} <button onClick={ () => deleteItem(item.id) }>X</button>
+                </li>
+              ))
+          }
+        </ul>
+        <form>
+          <input type="text" ref={nameInput} />
+          <button onClick={addItem}>add item</button>
+        </form>
+    </div>
+  )
+}
+
+export default UseStateWithArr
