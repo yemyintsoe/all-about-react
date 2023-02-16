311548c26bcc62520347d614f0f27f3bc606a313
Ye Myint Soe <52601159+yemyintsoe@users.noreply.github.com>
2023-02-16 14:21:38 +0630

    Create hooks08.useCallBack.js

---
+++ hooks08.useCallBack.js
@@ -0,0 +1,25 @@
+// React Memo is a higher-order component that wraps around a component to memoize the rendered output and avoid unnecessary renderings. This improves performance because it memoizes the result and skips rendering to reuse the last rendered result.
+
+// App.js
+use { useState, useCallBack } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+  
+  // main point goes here
+  const increment = useCallback(() => {
+    setCount(count + 1)
+  }, [count])
+  
+  return ( <Child /> )
+
+}
+
+
+// Child.js
+
+const Child = () => {
+    return ()
+}
+// main point goes here
+export default React.memo(Child)
