8565b00a67b093890b2385f700007177ae7befb2
Ye Myint Soe <52601159+yemyintsoe@users.noreply.github.com>
2024-02-19 11:54:47 +0630

    Update 01.installation.js

--- basics/01.installation.js
+++ basics/01.installation.js
@@ -1,6 +1,9 @@
 // we can implement the installation & creation of react app just as the following processes
 
 > npm install create-react-app
+  or
+> npm install -g npx
+
 > npx create-react-app first-app
 
 > npm start
