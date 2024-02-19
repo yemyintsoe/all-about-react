710f9c53f1887ce79425c3344541ef1f01434007
Ye Myint Soe <52601159+yemyintsoe@users.noreply.github.com>
2024-02-19 14:52:39 +0630

    Create react.js

---
+++ react18/react.js
@@ -0,0 +1,33 @@
+// 01.INSTALLATION
+> npm install -g npx
+> npx create-react-app react-demo
+or
+> npm create vite@latest
+
+//02. FUNCTIONAL COMPONENT WITH PROPS
+// components/Greet.js
+export const Greet = (props) => {
+    return(
+        <div>
+            <h1>Hello {props.name} - {props.age}</h1>
+            {props.children}
+        </div>
+    )
+}
+
+// App.js
+import {Greet} from './components/Greet'
+
+function App() {
+  return (
+    <div className="App">
+      <Greet name="YMS" age="25" />
+      <Greet name="WILLIAM" age="20" />
+      <Greet name="JOHN" age="26">
+        <p>what the hell is that bro ... </p>
+      </Greet>
+    </div>
+  );
+}
+
+// props cannot be modified
