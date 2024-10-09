e7edc698b0d122d723b77c777f83ccaf8be11016
Ye Myint Soe <52601159+yemyintsoe@users.noreply.github.com>
2024-10-09 10:15:39 +0630

    Update 11-useState.js

--- gonas/basic/11-useState.js
+++ gonas/basic/11-useState.js
@@ -1,4 +1,44 @@
-const [name, setName] = useState("")
+const [count, setCount] = useState(0);
 
-// name is a const variable
-// setName is a function to modify name variable
+/*
+# DESCRIPTION
+- count is a state or (const variable)
+- setCount is a function to modify count state
+*/
+
+/*
+# HOW TO USE
+- To output
+*/
+<h1>{ count }</h1>
+
+/* 
+- To modify
+*/
+const incrementCount = () => {
+    setCount(count + 1);
+  };
+<button onClick={incrementCount}>Increase</button>
+
+
+
+/*
+# === UPDATING STATE BASED ON CURRENT STATE ===
+# This won't increase two time
+const incrementCount = () => {
+  setCount(count + 1);
+   setCount(count + 1);
+};
+*/
+
+/*
+# But, this will
+*/
+const incrementCount = () => {
+  setCount((c) => c + 1);
+  setCount((c) => c + 1);
+};
+
+/*
+Sidenote: So that, to modify the state, we have to use the second approach (callback function)
+*/
